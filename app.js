58548e56366110fd020b8aec1fe27db4f9d2358a
Marc Gruner <marcjamiegruner@gmail.com>
2022-01-24 15:42:43 +0100

    chore: created a read file

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 // import { promises as fs } from "fs";
-import { writeFile, readFile } from "fs/promises";
+import { writeFile } from "fs/promises";
 
 console.log("Hello world");
 
 writeFile("foo", " anderes");
 
 writeFile("hello.json", JSON.stringify({ x: 5, y: 6 }, null, 4));
-
-readFile("hello.json", "utf8").then((data) => {
-  console.log(JSON.parse(data));
-});
